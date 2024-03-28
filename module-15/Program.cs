55ea5ce66167da68e66b0512a3f8e7883e6d1411
youknovv <99376197+youknovv@users.noreply.github.com>
2024-03-29 03:23:39 +0700

    add 15

---
+++ module-15/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var classes = new[]
+        {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+        var allStudents = GetAllStudents(classes);
+
+        Console.WriteLine(string.Join(" ", allStudents));
+    }
+
+    static IEnumerable<string> GetAllStudents(Classroom[] classes)
+    {
+        return classes.SelectMany(c => c.Students);
+    }
+
+    public class Classroom
+    {
+        public List<string> Students = new List<string>();
+    }
+}
